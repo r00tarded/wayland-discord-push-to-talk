f56c3b84ef0682d61c3b33be8730b547c73d342c
stanley2058 <stanley2058@yahoo.com.tw>
2022-02-20 21:14:02 +0800

    first commit

---
+++ PTTServer.plugin.js
@@ -0,0 +1,197 @@
+/**
+ * @name PTTServer
+ * @invite undefined
+ * @authorLink undefined
+ * @donate undefined
+ * @patreon undefined
+ * @website https://github.com/stanley2058/wayland-discord-push-to-talk
+ * @source undefined
+ */
+/*@cc_on
+@if (@_jscript)
+	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
+
+@else@*/
+
+module.exports = (() => {
+  const config = {
+    main: "index.js",
+    info: {
+      name: "PTTServer",
+      authors: [
+        {
+          name: "stanley2058",
+          discord_id: "194799696990961664",
+          github_username: "stanley2058",
+        },
+      ],
+      version: "1.0.0",
+      description: "A dirty workaround to achieve PTT on wayland",
+      github: "https://github.com/stanley2058/wayland-discord-push-to-talk",
+      github_raw: "",
+    },
+    changelog: [],
+  };
+  
+  // these two function are copied from Arashiryuu's plugin
+  // check it out: https://github.com/Arashiryuu/crap/blob/master/ToastIntegrated/HideServersChannelsRedux/HideServersChannelsRedux.plugin.js
+  const log = function () {
+    const parts = [
+      `%c[${config.info.name}]%c \u2014 %s`,
+      "color: #3A71C1; font-weight: 700;",
+      "",
+      new Date().toUTCString(),
+    ];
+    console.group.apply(null, parts);
+    console.log.apply(null, arguments);
+    console.groupEnd();
+  };
+  const error = function () {
+    const parts = [
+      `%c[${config.info.name}]%c \u2014 %s`,
+      "color: #3A71C1; font-weight: 700;",
+      "",
+      new Date().toUTCString(),
+    ];
+    console.group.apply(null, parts);
+    console.error.apply(null, arguments);
+    console.groupEnd();
+  };
+
+  return !global.ZeresPluginLibrary
+    ? class {
+        constructor() {
+          this._config = config;
+        }
+        getName = () => config.info.name;
+        getAuthor = () => config.info.authors.map((a) => a.name).join(", ");
+        getDescription = () => config.info.description;
+        getVersion = () => config.info.version;
+        load() {
+          BdApi.showConfirmationModal(
+            "Library Missing",
+            `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+            {
+              confirmText: "Download Now",
+              cancelText: "Cancel",
+              onConfirm: () => {
+                require("request").get(
+                  "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+                  async (error, response, body) => {
+                    if (error)
+                      return require("electron").shell.openExternal(
+                        "https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+                      );
+                    await new Promise((r) =>
+                      require("fs").writeFile(
+                        require("path").join(
+                          BdApi.Plugins.folder,
+                          "0PluginLibrary.plugin.js"
+                        ),
+                        body,
+                        r
+                      )
+                    );
+                  }
+                );
+              },
+            }
+          );
+        }
+        start() {}
+        stop() {}
+      }
+    : (([Plugin, Api]) => {
+        const plugin = (Plugin, Library) => {
+          const http = require("http");
+          const PORT = 3030;
+          const btnSelector = "section[class^='panels'] button[role='switch']";
+          const getMuteBtn = () => document.querySelector(btnSelector);
+          let server = null;
+
+          let dirtiness = 0;
+          const timer = setInterval(() => {
+            if (dirtiness > 0) {
+              dirtiness--;
+              if (dirtiness === 0) {
+                const btn = getMuteBtn();
+                if (btn && btn.ariaChecked === "false") {
+                  btn.click();
+                }
+              }
+            }
+          }, 50);
+
+          return class PTTServer extends Plugin {
+            onStart() {
+              const muteBtn = getMuteBtn();
+              server = http.createServer((req, res) => {
+                switch (req.url) {
+                  case "/start":
+                    if (muteBtn && muteBtn.ariaChecked === "true") {
+                      muteBtn.click();
+                    }
+                    res.writeHead(200);
+                    break;
+                  case "/stop":
+                    if (muteBtn && muteBtn.ariaChecked === "false") {
+                      muteBtn.click();
+                    }
+                    res.writeHead(200);
+                    break;
+                  case "/toggle":
+                    if (muteBtn) muteBtn.click();
+                    res.writeHead(200);
+                    break;
+                  case "/smart":
+                    // initially set dirtiness higher because mouse event takes time to ramp up
+                    if (dirtiness === 0) dirtiness = 15;
+                    else dirtiness = 5;
+
+                    if (muteBtn && muteBtn.ariaChecked === "true") {
+                      muteBtn.click();
+                    }
+                    res.writeHead(200);
+                    break;
+                  default:
+                    res.writeHead(404);
+                    break;
+                }
+                res.end();
+              });
+
+              server.listen(PORT, () => {
+                log(`PTT server listening on port: ${PORT}`);
+              });
+            }
+
+            onStop() {
+              clearInterval(timer);
+              server.close(() => {
+                log("PTT server stopped");
+              });
+            }
+          };
+        };
+        return plugin(Plugin, Api);
+      })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
---
+++ README.md
@@ -0,0 +1,25 @@
+# Wayland Discord Push To Talk
+
+> This is a workaround to mimic the push-to-talk functionality of Discord on Wayland.
+
+Since Wayland does not allow global keybindings, this plugin spins up a Node.js server in the background and listen on port `3030`. Once received a request, it then change the mute state of Discord accordingly.
+
+## Installation
+
+1. Install [BetterDiscord](https://github.com/BetterDiscord/Installer).
+2. Download `PTTServer.plugin.js` and put it in the `plugins` folder.
+3. Enable the plugin.
+4. Setup a keybinding in your compositor to send a request to the server.
+
+   e.g. This is my setup in KDE, I mapped one of my mouse buttons to the `F14` key, and then map `F14` to send a `curl` request to `localhost:3000/smart`.
+   ![](./img/kde_shortcuts.png)
+   KDE does not allow bindings on `keydown` and `keyup`, so I have to do it like this, press and hold the button will cause the action to be run repeatedly. In this case, it will cause repeating `curl` requests to the server, the `/smart` endpoint is designed specifically to handle this.
+
+5. Finally, make sure your Discord is in **Voice Activity** mode.
+
+## API
+
+- `/smart` - The recommended way to push-to-talk, this endpoint needs to be called repeatedly during speaking to keep the client unmute, and will switch to mute state automatically after stopped requesting.
+- `/toggle` - Toggle the mute state.
+- `/start` - Unmute.
+- `/stop` - Mute.
